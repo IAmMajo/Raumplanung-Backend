e260532330218353bbf08975918330c66b1f6826
Majo <git@maxoverlack.dev>
2021-02-02 11:13:15 +0100

    Add mysql

--- index.mjs
+++ index.mjs
@@ -1,18 +1,32 @@
 import { existsSync, readFileSync } from "fs";
+import { createConnection } from "mysql";
 import express from "express";
 
 const EXIT_CODE_NO_SETTINGS = 1;
 
+let connection = null;
 if (existsSync("settings.json")) {
   const settings = JSON.parse(readFileSync("settings.json", "utf-8"));
-  const app = express();
-  app.use(express.json());
-  app.listen(settings.port, () =>
-    process.stdout.write(
-      "Raumplanung Backend wurde mit den folgenden Einstellungen gestartet:\n" +
-        `Port: ${settings.port}\n`
-    )
-  );
+  connection = createConnection(settings.database);
+  connection.connect((err) => {
+    if (err) {
+      throw err;
+    }
+    const app = express();
+    app.use(express.json());
+    app.listen(settings.port, () =>
+      process.stdout.write(
+        "Raumplanung Backend wurde mit den folgenden Einstellungen" +
+          " gestartet:\n" +
+          `Port: ${settings.port}\n` +
+          `Datenbank-Host: ${settings.database.host}\n` +
+          `Datenbank-Port: ${settings.database.port}\n` +
+          `Datenbank-Name: ${settings.database.database}\n` +
+          `Datenbank-Benutzer: ${settings.database.user}\n` +
+          `Datenbank-Passwort: ${settings.database.password}\n`
+      )
+    );
+  });
 } else {
   process.stderr.write(
     "„settings.json“ existiert nicht! Bitte fertigen Sie eine Kopie der" +
@@ -21,3 +35,5 @@ if (existsSync("settings.json")) {
   );
   process.exitCode = EXIT_CODE_NO_SETTINGS;
 }
+
+export default connection;
